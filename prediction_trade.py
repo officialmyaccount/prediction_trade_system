27831c4027b4db650f02366a4ea423f6bc91cd43
officialmyaccount <bicebc@gmail.com>
2023-06-22 13:09:36 +0900

    グラフを１つにまとめる

--- prediction_trade.py
+++ prediction_trade.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
+import mplfinance as mpf
 from sklearn.metrics import r2_score
 from pandas_datareader import data as pdr
 from datetime import datetime
@@ -17,14 +18,6 @@ s_target = 'GOOG'
 df = pdr.get_data_yahoo(s_target, start = '2014-01-01', end = datetime.now())
 df.head()
 
-# 株価をグラフで表示
-plt.figure(figsize=(16, 6))
-plt.title(s_target + 'Close Price History')
-plt.plot(df['Close'])
-plt.xlabel('Date', fontsize = 14)
-plt.ylabel('Close Price USD($)', fontsize = 14)
-plt.show()
-
 # 移動平均線の表示
 ma_day = [10, 20, 50]
 for ma in ma_day:
@@ -32,11 +25,11 @@ for ma in ma_day:
     df[column_name] = df['Adj Close'].rolling(ma).mean()
 
 plt.figure(figsize=(16, 6))
-plt.title(s_target + 'Close Price MA History')
-plt.plot(df['Close'][-300:])
-plt.plot(df['MA for 10 days'][-300:])
-plt.plot(df['MA for 20 days'][-300:])
-plt.plot(df['MA for 50 days'][-300:])
+plt.title(s_target + 'Close Price History')
+plt.plot(df['Close'][-365:])
+plt.plot(df['MA for 10 days'][-365:])
+plt.plot(df['MA for 20 days'][-365:])
+plt.plot(df['MA for 50 days'][-365:])
 plt.xlabel('Date', fontsize = 14)
 plt.ylabel('Close Price USD($)', fontsize = 14)
 plt.legend(['Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days'], loc='upper right')
